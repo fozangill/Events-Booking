046b5f76a8c24dc61147a14209d12a4ab4ee3a86
fozangill <65424237+fozangill@users.noreply.github.com>
2023-07-20 13:52:56 +0200

    form creation for filtering data

--- index.php
+++ index.php
@@ -28,8 +28,66 @@ $dataInsertion = new DataInsertion($dbConnection->getConnection(), $employeesTab
 
 $dataInsertion->insertData($dataArray);
 
+
+
+// Getting the input filter values
+$employeeNameFilter = isset($_GET['employee_name']) ? $_GET['employee_name'] : '';
+$eventIDFilter = isset($_GET['event_name']) ? $_GET['event_name'] : '';
+$eventDateFilter = isset($_GET['event_date']) ? $_GET['event_date'] : '';
+
+
 // Closing the connection
 $dbConnection->getConnection()->close();
-
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Filtered Results</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th, td {
+            text-align: left;
+            padding: 8px;
+            border-bottom: 1px solid #ddd;
+        }
+    </style>
+</head>
+<body>
+    <h1>Filter Results</h1>
+
+    <form method="GET" action="">
+        <label for="employee">Employee Name:</label>
+        <input type="text" id="employee_name" name="employee_name" value="<?= htmlspecialchars($employeeNameFilter) ?>">
+
+        <label for="event">Event Name:</label>
+        <input type="text" id="event_name" name="event_name" value="<?= htmlspecialchars($eventIDFilter) ?>">
+
+        <label for="date">Event Date:</label>
+        <input type="date" id="event_date" name="event_date" value="<?= htmlspecialchars($eventDateFilter) ?>">
+
+        <button type="submit">Filter</button>
+    </form>
+
+    <table>
+        <tr>
+            <th>Participation ID</th>
+            <th>Employee Name</th>
+            <th>Employee Mail</th>
+            <th>Event ID</th>
+            <th>Event Name</th>
+            <th>Participation Fee</th>
+            <th>Event Date</th>
+            <th>Version</th>
+        </tr>
+
+    </table>
+
+
+
+</body>
+</html>
