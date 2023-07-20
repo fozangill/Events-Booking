f4f4662d45be805298affe2f64bd5c17e2b36c53
fozangill <65424237+fozangill@users.noreply.github.com>
2023-07-20 13:58:49 +0200

    Filtering Data function

---
+++ FilteringData.php
@@ -0,0 +1,41 @@
+<?php 
+
+class FilteringData {
+	
+	private $connection;
+	private $fetchData;
+
+    public function __construct($connection) {
+        $this->connection = $connection;
+       
+    }
+
+    public function getFilteredResults($employeeNameFilter, $eventIDFilter, $eventDateFilter)
+    {
+        $employeeNameFilter = '%' . $employeeNameFilter . '%';
+        $eventIDFilter = '%' . $eventIDFilter . '%';
+        $eventDateFilter = '%' . $eventDateFilter . '%';
+
+        $query = "SELECT employees.employee_name, participations.participation_id, events.event_name, participations.participation_fee, events.event_date, participations.event_id, participations.employee_mail, participations.version
+                  FROM participations
+                  INNER JOIN employees ON participations.employee_mail = employees.employee_mail
+                  INNER JOIN events ON participations.event_id = events.event_id
+                  WHERE employees.employee_name LIKE '$employeeNameFilter'
+                    AND events.event_name LIKE '$eventIDFilter'
+                    AND events.event_date LIKE '$eventDateFilter'";
+
+        $result = $this->connection->query($query);
+
+        $filteredParticipations = [];
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $filteredParticipations[] = $row;
+            }
+        }
+
+        return $filteredParticipations;
+    }
+
+
+
+}
