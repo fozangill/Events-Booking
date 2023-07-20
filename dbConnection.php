4d5bbb56292cad96249238f784f6543e95c3d8ee
fozangill <65424237+fozangill@users.noreply.github.com>
2023-07-20 13:14:11 +0200

    Database Connection file

---
+++ dbConnection.php
@@ -0,0 +1,28 @@
+<?php 
+
+class DBConnection {
+
+    private $servername;
+    private $username;
+    private $password;
+    private $dbname;
+    private $connection;
+
+    public function __construct($servername, $username, $password, $dbname) {
+        $this->servername = $servername;
+        $this->username = $username;
+        $this->password = $password;
+        $this->dbname = $dbname;
+    }
+
+    public function connect() {
+        $this->connection = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+        if ($this->connection->connect_error) {
+            die("Connection failed: " . $this->connection->connect_error);
+        }
+    }
+
+    public function getConnection() {
+        return $this->connection;
+    }
+}
